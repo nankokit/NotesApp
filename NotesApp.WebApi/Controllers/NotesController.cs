2d9c75d1d97f927870b121f4adcdccb933e76a67
nankokit <nanko_k@mail.ru>
2025-07-18 16:40:49 +0300

    added controllers

---
+++ NotesApp.WebApi/Controllers/NotesController.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NotesApp.Application.Commands;
+using NotesApp.Application.DTOs;
+using NotesApp.Application.Queries;
+
+namespace NotesApp.WebApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class NotesController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public NotesController(IMediator mediator)
+    {
+        _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<PagedResult<NoteDto>>> GetAll([FromQuery] GetAllNotesQuery query, CancellationToken cancellationToken)
+    {
+        var result = await _mediator.Send(query, cancellationToken);
+        return Ok(result);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<NoteDto>> GetById(Guid id, CancellationToken cancellationToken)
+    {
+        var query = new GetNoteByIdQuery { Id = id };
+        var result = await _mediator.Send(query, cancellationToken);
+        return Ok(result);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<NoteDto>> Create([FromBody] CreateNoteCommand command, CancellationToken cancellationToken)
+    {
+        var result = await _mediator.Send(command, cancellationToken);
+        return CreatedAtAction(nameof(GetById), new { id = result.Id }, result);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Update(Guid id, [FromBody] UpdateNoteCommand command, CancellationToken cancellationToken)
+    {
+        if (id != command.Id)
+        {
+            return BadRequest("Route ID does not match command ID");
+        }
+
+        await _mediator.Send(command, cancellationToken);
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(Guid id, CancellationToken cancellationToken)
+    {
+        await _mediator.Send(new DeleteNoteCommand { Id = id }, cancellationToken);
+        return NoContent();
+    }
+}
---
+++ NotesApp.WebApi/Controllers/TagsController.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NotesApp.Application.Commands;
+using NotesApp.Application.DTOs;
+using NotesApp.Application.Queries;
+using NotesApp.Domain.Entities;
+
+namespace NotesApp.WebApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TagsController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public TagsController(IMediator mediator)
+    {
+        _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<TagDto>>> GetAll(CancellationToken cancellationToken)
+    {
+        var result = await _mediator.Send(new GetAllTagsQuery(), cancellationToken);
+        return Ok(result);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<TagDto>> GetById(Guid id, CancellationToken cancellationToken)
+    {
+        var result = await _mediator.Send(new GetTagByIdQuery { Id = id }, cancellationToken);
+        return Ok(result);
+    }
+
+    [HttpGet("by-name/{name}")]
+    public async Task<ActionResult<TagDto>> GetByName(string name, CancellationToken cancellationToken)
+    {
+        var result = await _mediator.Send(new GetTagByNameQuery { Name = name }, cancellationToken);
+        return Ok(result);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<TagDto>> Create([FromBody] CreateTagCommand command, CancellationToken cancellationToken)
+    {
+        var result = await _mediator.Send(command, cancellationToken);
+        return CreatedAtAction(nameof(GetById), new { id = result.Id }, result);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Update(Guid id, [FromBody] UpdateTagCommand command, CancellationToken cancellationToken)
+    {
+        if (id != command.Id)
+        {
+            return BadRequest("Route ID does not match command ID");
+        }
+        await _mediator.Send(command, cancellationToken);
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(Guid id, CancellationToken cancellationToken)
+    {
+        await _mediator.Send(new DeleteTagCommand { Id = id }, cancellationToken);
+        return NoContent();
+    }
+}
